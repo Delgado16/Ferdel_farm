0a741415893962b867d081249d72c2b38840e8cf
Delgado16 <juanindelgado950@gmail.com>
2026-01-26 09:43:35 -0600

    update layout

--- app.py
+++ app.py
@@ -428,6 +428,9 @@ def admin_required(f):
 def bodega_required(f):
     return role_requerido('Bodega')(f)
 
+def vendedor_required(f):
+    return role_requerido('Vendedor')(f)
+
 def admin_or_bodega_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -11526,6 +11529,8 @@ def bodega_reportes_avanzados():
     except Exception as e:
         flash(f"Error al cargar reportes: {str(e)}", 'error')
         return redirect(url_for('admin_historial_movimientos'))
+
+## Ventas vendedor
 
 
 #Iniciar Aplicación
