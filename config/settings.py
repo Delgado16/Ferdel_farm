d1c604106ad5ffa5c9b85a04e72c6a64071e9b7d
Delgado16 <juanindelgado950@gmail.com>
2026-09-08 15:51:16 -0600

    Railway version de prueba

--- config/settings.py
+++ config/settings.py
@@ -68,9 +68,9 @@ DB_SSL_CA = os.environ.get('DB_SSL_CA', None)
 DB_CONFIG = {
     'user': os.environ.get('DB_USER', 'root'),
     'password': os.environ.get('DB_PASSWORD', ''),
-    'host': os.environ.get('DB_HOST', 'shinkansen.proxy.rlwy.net'),
-    'port': int(os.environ.get('DB_PORT', 40144)),
-    'database': os.environ.get('DB_NAME', 'db_ferdel'),
+    'host': os.environ.get('DB_HOST', 'altaria.proxy.rlwy.net'),
+    'port': int(os.environ.get('DB_PORT', 29240)),
+    'database': os.environ.get('DB_NAME', 'railway'),
     'pool_name': 'ferdel_pool',
     'pool_size': int(os.environ.get('DB_POOL_SIZE', 5)),
     'pool_reset_session': True,
